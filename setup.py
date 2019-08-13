2b26b492561d9babe768deb73c271ec97cf0aa35
Marc Roig Vilamala <marc.roig.vilamala@gmail.com>
2019-08-13 11:10:28 +0100

    Added a setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ProblogPreCompilation',
+    version='0.1dev',
+    packages=['preCompilation'],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
